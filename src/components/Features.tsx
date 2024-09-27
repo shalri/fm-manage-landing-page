97c63ccdbd3782c603030f3a50e9a2ed42d4d918
shalri <fsaonlinework777@gmail.com>
2024-09-27 17:43:38 +0800

    Features mobile layout

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -2,18 +2,30 @@ import { featuresCopy, manageInfoCopy } from "@/libs/data";
 
 export default function Features() {
   return (
-    <article>
-      <h2 className="">{manageInfoCopy.heading}</h2>
-      <p className="">{manageInfoCopy.body}</p>
+    <article className="mt-[98px]">
+      <div className="space-y-3 px-8 text-center">
+        <h2 className="text-pretty text-[1.75rem] font-bold leading-[1.6] text-ml-dark-blue">
+          {manageInfoCopy.heading}
+        </h2>
+        <p className="text-sm leading-[2] text-ml-dark-grayish-blue">
+          {manageInfoCopy.body}
+        </p>
+      </div>
 
-      <div className="">
+      <div className="mt-[3.25rem] space-y-[2.875rem] pl-4">
         {featuresCopy.map((feature) => (
           <section className="" key={feature.id}>
-            <div className="">
-              {feature.id}
-              <h3 className="">{feature.heading}</h3>
+            <div className="flex items-center bg-ml-very-pale-red">
+              <span className="block content-center rounded-full bg-ml-bright-red px-6 py-[0.4375rem] font-bold text-white">
+                {feature.id}
+              </span>
+              <h3 className="px-4 font-bold -tracking-[0.055em] text-ml-dark-blue">
+                {feature.heading}
+              </h3>
             </div>
-            <p className="">{feature.body}</p>
+            <p className="mt-[0.625rem] pr-4 text-sm leading-[2] -tracking-[0.015em] text-ml-dark-grayish-blue">
+              {feature.body}
+            </p>
           </section>
         ))}
       </div>
